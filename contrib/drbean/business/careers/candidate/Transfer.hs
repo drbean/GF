240339869c2ef7fc863f7ce36006e260384a341f
Dr Bean <drbean@localhost.localdomain>
2013-08-31 22:12:52 +0900

    /home/drbean/bett/branch/gf/bin/Candidate.pgf.re-linearize_parse.lc_first,comp_dot

--- contrib/drbean/business/careers/candidate/Transfer.hs
+++ contrib/drbean/business/careers/candidate/Transfer.hs
@@ -3,17 +3,36 @@ module Main where
 import PGF
 import Candidate
 
+import Data.List
+import Data.Char
+
 main :: IO () 
 main = do
-  gr <- readPGF "Candidate.pgf"
+  gr <- readPGF "/home/drbean/bett/branch/gf/bin/Candidate.pgf"
   s <- getLine
-  putStrLn ((translate transfer gr) s)
+  let l = lc_first s
+  let ls = linear transform gr (parses l gr)
+  putStrLn (concat ls)
 
-translate :: (Tree -> Tree) -> PGF -> String -> String
-translate tr gr s = show $ concat ( parseAll gr (startCat gr) s )
+parses :: String -> PGF -> [Tree]
+parses s gr = concat ( parseAll gr (startCat gr) s )
 
-transfer :: Tree -> Tree
-transfer = gf . answer . fg
+transform :: Tree -> Tree
+transform = gf . answer . fg
 
 answer :: GCl -> GCl
 answer = id
+
+linear :: (Tree -> Tree) -> PGF -> [Tree] -> [ String ]
+linear tr gr ps = concat $ map ((linearizeAll gr) . tr) ps
+
+lc_first :: String -> String
+lc_first str@(s:ss) = case ( or $ map (flip isPrefixOf str) ["Barbara", "Tadeusz", "Eva", "Dr Bean"] ) of
+	True  -> (s:ss)
+	False -> ((toLower s):ss)
+
+chomp :: String -> String
+chomp str = let rev@(c:cs) = reverse string
+		in dot = case (c:cs) of
+			'.':_ -> cs
+			otherwise -> rev
